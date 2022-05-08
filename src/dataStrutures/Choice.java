b62bcc4e6773fe5fdea80708014e94ef111387e2
Matteo <matteodinoia02@gmail.com>
2022-05-08 23:15:53 +0200

    modifying minor writing mistake

--- src/dataStrutures/Choice.java
+++ src/dataStrutures/Choice.java
@@ -5,7 +5,7 @@ public class Choice <E>{
 	
 	//INDEX SELECTED  --------------------------------------------------
 	private Integer indexChoosenElement; 
-	/**must be always between 0 and the list length - 1    or   null
+	/**Must be always between 0 and the list length - 1    or   null
 	 * @return true if it actually change the index, false otherwise  */
 	public boolean choose(Integer index) {
 		if(index==null || (index>=0 && index<elementsToChooseIn.length)) {
