ff9455be7b6cdd6a1d4e89f1520a301ccda303f8
Matteo <matteodinoia02@gmail.com>
2022-05-08 23:04:43 +0200

    modifying class Choice

--- src/dataStrutures/Choice.java
+++ src/dataStrutures/Choice.java
@@ -1,5 +1,22 @@
 package dataStrutures;
 
-public class Choice {
-
+public class Choice <E>{
+	private E[] elementsToChooseIn;
+	
+	
+	private Integer indexChoosenElement; 
+	/**must be always between 0 and the list length - 1    or   null
+	 * @return true if it actually change the index, false otherwise  */
+	public boolean choose(Integer index) {
+		if(index==null || (index>=0 && index<elementsToChooseIn.length)) {
+			indexChoosenElement=index;
+			return true;
+		}
+		
+		return false;
+	}
+	public E getChoosenElement() {
+		if(indexChoosenElement==null) return null;
+		return elementsToChooseIn[indexChoosenElement];
+	}
 }
